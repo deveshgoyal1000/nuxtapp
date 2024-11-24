9395799ad748fcec3c35bf1fbcf5ecace7eb71b4
caacademy <devesh.21bcon427@jecrcu.edu.in>
2024-11-25 01:40:28 +0530

    '1.1'

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,21 @@
 <template>
-  <div>
+  <div class="p-4">
     <h1 class="text-2xl font-bold">Medical Imaging Tool</h1>
     <input type="file" @change="handleFileUpload" />
+    <Toolbar @onToolSelect="onToolSelect" />
+    <Canvas />
   </div>
 </template>
 
 <script setup>
-const handleFileUpload = (e) => {
-  const file = e.target.files[0];
-  console.log('File Uploaded:', file);
+import Toolbar from '~/components/Toolbar.vue';
+import Canvas from '~/components/Canvas.vue';
+
+const handleFileUpload = (event) => {
+  const file = event.target.files[0];
+  console.log('Uploaded File:', file);
+};
+const onToolSelect = (tool) => {
+  console.log('Selected Tool:', tool);
 };
 </script>
