3a5f1af1e86c4242e6e16ba7b4cf6b3e1cd454a2
caacademy <devesh.21bcon427@jecrcu.edu.in>
2024-11-25 01:59:51 +0530

    '1.1'

--- components/Toolbar.vue
+++ components/Toolbar.vue
@@ -1,7 +1,17 @@
 <template>
-  <div class="toolbar flex gap-2">
-    <button @click="$emit('onToolSelect', 'zoom')">Zoom</button>
-    <button @click="$emit('onToolSelect', 'crop')">Crop</button>
-    <button @click="$emit('onToolSelect', 'adjust')">Adjust</button>
+  <div class="toolbar">
+    <!-- Add buttons for tools like zoom, brightness, crop, etc. -->
+    <button @click="zoomIn">Zoom In</button>
+    <button @click="zoomOut">Zoom Out</button>
   </div>
 </template>
+
+<script setup>
+// Define methods like zoomIn, zoomOut, etc.
+const zoomIn = () => { /* logic for zooming in */ }
+const zoomOut = () => { /* logic for zooming out */ }
+</script>
+
+<style scoped>
+/* Add toolbar styles here */
+</style>
