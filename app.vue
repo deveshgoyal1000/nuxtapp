99d974bf083bd465e3526e346d799dc1fc8007d2
caacademy <devesh.21bcon427@jecrcu.edu.in>
2024-11-24 23:34:21 +0530

    '1.1'

--- app.vue
+++ app.vue
@@ -1,14 +1,44 @@
 <template>
-  <div>
-    <Navbar />
-    <NuxtPage />
+  <div id="app">
+    <div>
+      <h1>Dynamic Image Editor</h1>
+      <button @click="changeImage">Change Image</button>
+    </div>
+    <ImageEditor :imageSrc="imageUrl" />
   </div>
 </template>
 
-<script setup>
-import Navbar from '~/components/Navbar.vue';
+<script>
+import { ref } from 'vue';
+import ImageEditor from './components/ImageEditor.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ImageEditor,
+  },
+  setup() {
+    // Initial image source
+    const imageUrl = ref('https://via.placeholder.com/800x600.png?text=Initial+Image');
+
+    // Function to change the image source dynamically
+    const changeImage = () => {
+      imageUrl.value =
+        'https://via.placeholder.com/800x600.png?text=New+Image';
+    };
+
+    return {
+      imageUrl,
+      changeImage,
+    };
+  },
+};
 </script>
 
 <style>
-/* Add global styles here if needed */
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  margin-top: 20px;
+}
 </style>
