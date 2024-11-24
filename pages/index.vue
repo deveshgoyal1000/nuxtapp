8dde586fa92d5442afe4af69ec319f27ad8c9447
caacademy <devesh.21bcon427@jecrcu.edu.in>
2024-11-24 20:42:19 +0530

    '1.1'

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
-  <div class="flex flex-col items-center justify-center h-screen bg-gray-100">
-    <h1 class="text-4xl font-bold text-blue-500">Medical Imaging App</h1>
-    <p class="mt-4 text-gray-600">Start building your medical image tools here.</p>
+  <div class="h-screen bg-blue-500 text-white flex items-center justify-center">
+    <h1 class="text-4xl font-bold">Hello, Tailwind with Nuxt!</h1>
   </div>
 </template>
