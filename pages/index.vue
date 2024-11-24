9cb685c97a4bbbfcdf1c029246b44d2a2fa4483e
caacademy <devesh.21bcon427@jecrcu.edu.in>
2024-11-24 19:02:39 +0530

    '1.1'

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,19 @@
-<template>
+<template> 
   <div class="flex flex-col items-center justify-center h-screen bg-gray-100">
     <h1 class="text-4xl font-bold text-blue-500">Medical Imaging App</h1>
     <p class="mt-4 text-gray-600">Start building your medical image tools here.</p>
+    
+    <!-- Add the DICOM Viewer Below -->
+    <DicomViewer />
   </div>
 </template>
+
+<script>
+import DicomViewer from "~/components/DicomViewer.vue";
+
+export default {
+  components: {
+    DicomViewer,
+  },
+};
+</script>
