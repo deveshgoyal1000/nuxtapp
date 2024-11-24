f2f69137cbc9363e8356dbd3a6e3134145c7544e
caacademy <devesh.21bcon427@jecrcu.edu.in>
2024-11-25 01:40:02 +0530

    '1.1'

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,13 @@
 <template>
-  <div class="p-4">
-    <h1 class="text-xl font-bold">Medical Imaging Tool</h1>
-    <input type="file" @change="handleFileUpload" accept=".png,.dcm" />
+  <div>
+    <h1 class="text-2xl font-bold">Medical Imaging Tool</h1>
+    <input type="file" @change="handleFileUpload" />
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-const handleFileUpload = (event) => {
-  const file = event.target.files[0];
-  console.log('Uploaded File:', file);
-  // Handle file logic here
+const handleFileUpload = (e) => {
+  const file = e.target.files[0];
+  console.log('File Uploaded:', file);
 };
 </script>
