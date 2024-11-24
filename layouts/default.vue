268db13ac53b9338b368d32ab95a7eeb517bbe05
caacademy <devesh.21bcon427@jecrcu.edu.in>
2024-11-25 01:33:36 +0530

    '1.1'

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,21 @@
 <template>
     <div>
-      <NuxtPage />
+      <header>
+        <!-- Your header content here -->
+      </header>
+      
+      <!-- This is where the page content will be injected -->
+      <main>
+        <NuxtPage />
+      </main>
+      
+      <footer>
+        <!-- Your footer content here -->
+      </footer>
     </div>
   </template>
+  
+  <script setup>
+  // Optionally, you can define meta data or props here
+  </script>
   
