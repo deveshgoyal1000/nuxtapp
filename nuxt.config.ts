76258a48692d89bd3e637cef2b838476886505ea
caacademy <devesh.21bcon427@jecrcu.edu.in>
2024-11-25 01:39:31 +0530

    '1.1'

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,3 @@
 export default defineNuxtConfig({
-  pages: true,   // Ensure that Nuxt's pages system is enabled
-  layouts: true, // Ensure that Nuxt's layouts system is enabled
-  // You can add other configurations here as needed
-})
+  modules: ['@nuxtjs/tailwindcss'],
+});
