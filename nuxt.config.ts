d40c92c190d1aace7191e77a7656397f6d588ed2
caacademy <devesh.21bcon427@jecrcu.edu.in>
2024-11-25 02:59:16 +0530

    '1.1'

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,3 @@
 export default defineNuxtConfig({
-  css: ['@/assets/styles/tailwind.css'],
-  modules: ['@pinia/nuxt', '@nuxt/image-edge', 'naive-ui/nuxt'],
-  build: {
-    transpile: ['naive-ui']
-  },
-  runtimeConfig: {
-    public: {
-      apiUrl: process.env.API_URL || 'http://localhost:3000',
-    },
-  },
+  modules: ['@pinia/nuxt'],
 });
