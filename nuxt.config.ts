17e1df0d49d17ae3e1af696d498930fcc8f6686d
caacademy <devesh.21bcon427@jecrcu.edu.in>
2024-11-25 01:58:11 +0530

    '1.1'

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
+import { defineNuxtConfig } from 'nuxt3'
+
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
   css: ['~/assets/css/tailwind.css'],
-});
+  build: {
+    transpile: ['naive-ui'],
+  },
+  tailwindcss: {
+    viewer: true, // Enable Tailwind viewer to view CSS styles in dev
+  },
+  plugins: [
+    { src: '~/plugins/naive-ui.js', mode: 'client' }
+  ],
+  // Any other configuration you may want to add
+})
