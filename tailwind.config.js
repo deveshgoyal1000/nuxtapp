028e517c2e112f249e29ab8966c2370a0b2b4041
caacademy <devesh.21bcon427@jecrcu.edu.in>
2024-11-25 01:58:24 +0530

    '1.1'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,17 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{vue,js}',  // Include all your page files
-    './layouts/**/*.{vue,js}', // Include all your layout files
-    './components/**/*.{vue,js}', // Include all your component files
-    './assets/css/**/*.{css,scss}', // Ensure this path includes the tailwind.css file
+    './pages/**/*.{vue,js,ts,jsx,tsx}',
+    './components/**/*.{vue,js,ts,jsx,tsx}',
+    './layouts/**/*.{vue,js,ts,jsx,tsx}',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#ff5733', // Example custom color
+      },
+    },
   },
   plugins: [],
 }
