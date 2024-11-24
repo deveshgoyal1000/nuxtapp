2f48c2653149f86b9a6116615e6698076687693f
caacademy <devesh.21bcon427@jecrcu.edu.in>
2024-11-25 01:49:54 +0530

    '1.1'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{vue,js}',
-    './components/**/*.{vue,js}',
-    './layouts/**/*.{vue,js}',
-    './app.vue',
+    './pages/**/*.{vue,js}',  // Include all your page files
+    './layouts/**/*.{vue,js}', // Include all your layout files
+    './components/**/*.{vue,js}', // Include all your component files
+    './assets/css/**/*.{css,scss}', // Ensure this path includes the tailwind.css file
   ],
   theme: {
     extend: {},
