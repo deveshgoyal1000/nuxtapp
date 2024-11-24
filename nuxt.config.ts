e1f6649ef8c1f01d3255f8eba4e48f24e3bd0550
caacademy <devesh.21bcon427@jecrcu.edu.in>
2024-11-24 20:11:32 +0530

    '1.1'

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,18 @@
+import { defineNuxtConfig } from 'nuxt';
+
 export default defineNuxtConfig({
+  css: ['~/assets/css/tailwind.css'],
   app: {
     head: {
       title: 'Medical Imaging App',
-      meta: [
-        { name: 'description', content: 'A Nuxt 3 application for medical imaging' }
-      ]
+      meta: [{ name: 'description', content: 'A Nuxt 3 app for medical imaging' }]
     }
-  }
-})
+  },
+  modules: ['@nuxt/postcss8'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
