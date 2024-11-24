533ba31103833d8e3c6058dc15c68eb46a135f0e
caacademy <devesh.21bcon427@jecrcu.edu.in>
2024-11-24 20:14:45 +0530

    '1.1'

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    <h1>Medical Imaging App</h1>
-    <p>Welcome to the Nuxt 3 medical imaging app starter project!</p>
+  <div class="text-center py-10">
+    <Header />
+    <h1 class="text-3xl font-bold mt-4">Welcome to Medical Imaging App</h1>
+    <p class="text-gray-600 mt-2">Explore cutting-edge medical imaging technologies</p>
   </div>
 </template>
+
+<script setup>
+import Header from '~/components/Header.vue';
+</script>
