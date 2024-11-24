862fe141dc359317ef64f11643a7ccc0dd7cd2ae
caacademy <devesh.21bcon427@jecrcu.edu.in>
2024-11-24 20:05:37 +0530

    '1.1'

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,6 @@
-<template> 
-  <div class="flex flex-col items-center justify-center h-screen bg-gray-100">
-    <h1 class="text-4xl font-bold text-blue-500">Medical Imaging App</h1>
-    <p class="mt-4 text-gray-600">Start building your medical image tools here.</p>
-    
-    <!-- Add the DICOM Viewer Below -->
-    <DicomViewer />
+<template>
+  <div>
+    <h1>Medical Imaging App</h1>
+    <p>Welcome to the Nuxt 3 medical imaging app starter project!</p>
   </div>
 </template>
-
-<script>
-import DicomViewer from "~/components/DicomViewer.vue";
-
-export default {
-  components: {
-    DicomViewer,
-  },
-};
-</script>
