35609226ebbbed11f01339e993eccb1c90aea19d
caacademy <devesh.21bcon427@jecrcu.edu.in>
2024-11-24 19:19:27 +0530

    '1.1'

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,3 @@
-// nuxt.config.js
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
@@ -6,10 +5,16 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
   build: {
     transpile: ['dcmjs', 'cornerstone-core', 'cornerstone-tools', 'dicom-parser'],
+    extend(config, { isClient, isServer }) {
+      if (isServer) {
+        config.externals = { canvas: 'commonjs canvas' };
+      }
+    },
   },
   vite: {
     optimizeDeps: {
       include: ['dcmjs', 'cornerstone-core', 'cornerstone-tools', 'dicom-parser'],
+      exclude: ['cornerstone-core'],  // Exclude cornerstone-core from Vite's optimization
     },
   },
 });
