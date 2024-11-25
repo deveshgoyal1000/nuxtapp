4821d5f82673a3d438047d178e990c5a42eb5447
caacademy <devesh.21bcon427@jecrcu.edu.in>
2024-11-25 20:02:39 +0530

    1.1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,12 @@
 export default defineNuxtConfig({
   css: [],
+
   postcss: {
     plugins: {
       autoprefixer: {},
       tailwindcss: {}, // Example if you're using Tailwind CSS
     },
   },
+
+  compatibilityDate: "2024-11-25",
 });
