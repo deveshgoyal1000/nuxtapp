2e736600552828700032dcaa5425bc20b97ea6be
caacademy <devesh.21bcon427@jecrcu.edu.in>
2024-11-25 20:11:29 +0530

    '1.1'

--- pages/viewer.vue
+++ pages/viewer.vue
@@ -1,17 +1,22 @@
 <template>
-    <div>
-      <Toolbar :onToolSelect="handleToolSelect" />
-      <Canvas :image="uploadedImage" />
-    </div>
-  </template>
-  
-  <script setup>
-  import Toolbar from '@/components/Toolbar.vue';
-  import Canvas from '@/components/Canvas.vue';
-  import { ref } from 'vue';
-  const uploadedImage = ref(null);
-  const handleToolSelect = (tool) => {
-    console.log(`Selected Tool: ${tool}`);
-  };
-  </script>
-  
+  <div>
+    <Toolbar :onToolSelect="handleToolSelect" />
+    <Canvas :image="uploadedImage" />
+  </div>
+</template>
+
+<script setup>
+import Toolbar from '@/components/Toolbar.vue';
+import Canvas from '@/components/Canvas.vue';
+import { ref } from 'vue';
+
+const uploadedImage = ref(null);
+
+const handleToolSelect = (tool) => {
+  console.log(`Selected Tool: ${tool}`);
+};
+</script>
+
+<style scoped>
+/* Add any custom styles for this component here */
+</style>
