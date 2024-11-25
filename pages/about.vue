9e0e15691b7a79d28183f372909bda5118f8036b
caacademy <devesh.21bcon427@jecrcu.edu.in>
2024-11-26 01:58:30 +0530

    '1.1'

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
-    <h1>About Page</h1>
-    <p>Welcome to the About page!</p>
+    <h1>About Us</h1>
+    <p>This application provides advanced tools for medical imaging and DICOM file management.</p>
   </div>
 </template>
+
+<script setup>
+</script>
+
+<style scoped>
+h1 {
+  text-align: center;
+  margin-top: 20px;
+}
+
+p {
+  text-align: center;
+  margin-top: 10px;
+  color: #555;
+}
+</style>
