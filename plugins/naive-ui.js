802f27ab895f43e0b313ffb7c0526a722f752741
caacademy <devesh.21bcon427@jecrcu.edu.in>
2024-11-25 20:11:53 +0530

    '1.1'

--- plugins/naive-ui.js
+++ plugins/naive-ui.js
@@ -2,6 +2,6 @@ import { defineNuxtPlugin } from '#app'
 import { create } from 'naive-ui'
 
 export default defineNuxtPlugin(nuxtApp => {
-  const naive = create()
-  nuxtApp.provide('naive', naive)
+  const naive = create() // Create an instance of Naive UI
+  nuxtApp.provide('naive', naive) // Inject it into the app context
 })
